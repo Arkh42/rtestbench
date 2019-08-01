7d229583900f7c1a54d44c94b79c2b2c08cb4e94
Arkh42 <aquenon@hotmail.be>
2019-08-01 15:25:48 +0200

    NEW: RTestBench class (first structure, no implementation)

---
+++ rtestbench/__init__.py
@@ -0,0 +1,29 @@
+
+# VISA protocol
+import visa
+
+# Scientific computations
+import numpy as np
+import pandas as pd
+
+
+
+class RTestBench(verbose_level):
+
+    """Manager for Remote Test Bench.
+    """
+
+
+    def __init__(self):
+        pass
+    
+    def __del__(self):
+        pass
+    
+
+    def detect_ressources(self):
+        pass
+    
+
+    def attach_ressource(self):
+        pass
