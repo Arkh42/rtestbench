148534b0b474f850a68d1a3693332be5ce9f6508
Arkh42 <aquenon@hotmail.be>
2020-01-30 17:07:52 +0100

    Make the _logger module consistent with the RTestBench spirit.
    CHANGE:
    - config() renamed make_logger() for consistency
    
    NEW:
    - verbose argument to set the level of the log stream
    - dostrings

--- rtestbench/_logger.py
+++ rtestbench/_logger.py
@@ -1,30 +1,39 @@
+"""A logger configurator for the application.
+
+Relies on the logging module.
+"""
+
 
 import logging
 
 
-def config(logger_name=''):
+def make_logger(logger_name: str = '', verbose: bool = True) -> logging.Logger:
+    """Returns a logger for handling log file and stream.
 
-    # set logger
+    Args:
+        logger_name: The name of the logger.
+        verbose: A boolean to choose between INFO and WARNING level printed in the terminal
+    """
+
     if logger_name:
         logger = logging.getLogger(logger_name)
     else:
         logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    # define handlers
     console_loghandler = logging.StreamHandler()
-    console_loghandler.setLevel(logging.INFO)
+    if verbose:
+        console_loghandler.setLevel(logging.INFO)
+    else:
+        console_loghandler.setLevel(logging.WARNING)
 
     file_loghandler = logging.FileHandler(filename='rtestbench.log', mode='w')
     file_loghandler.setLevel(logging.DEBUG)
 
-    # set format
     formatter = logging.Formatter('%(name)s: %(asctime)s - %(levelname)s - %(message)s')
-
     console_loghandler.setFormatter(formatter)
     file_loghandler.setFormatter(formatter)
 
-    # assign handlers
     logger.addHandler(console_loghandler)
     logger.addHandler(file_loghandler)
 
---
+++ rtestbench/tests/test_logger.py
@@ -0,0 +1,30 @@
+"""Unit tests for the _logger module."""
+
+
+import pytest
+from logging import DEBUG, INFO, WARNING
+
+import rtestbench._logger as log
+
+
+def test_make_logger_verbose():
+    """Tests that the verbose logger has the correct name and levels."""
+
+    logger = log.make_logger('toto_verb', verbose=True)
+
+    assert logger.name == 'toto_verb'
+    assert logger.level == DEBUG
+
+    assert logger.handlers[0].level == INFO
+    assert logger.handlers[1].level == DEBUG
+
+def test_make_logger_nonverbose():
+    """Tests that the non-verbose logger has the correct name and levels."""
+
+    logger = log.make_logger('toto_nverb', verbose=False)
+
+    assert logger.name == 'toto_nverb'
+    assert logger.level == DEBUG
+
+    assert logger.handlers[0].level == WARNING
+    assert logger.handlers[1].level == DEBUG
