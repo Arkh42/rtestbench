b227d118a1d77bcce1612ed82b7b218c2fd1297d
Arkh42 <aquenon@hotmail.be>
2020-05-27 11:19:14 +0200

    FIX: improve test coverage of _logger module.

--- rtestbench/tests/test_logger.py
+++ rtestbench/tests/test_logger.py
@@ -7,6 +7,17 @@ from logging import DEBUG, INFO, WARNING
 import rtestbench._logger as log
 
 
+def test_make_logger_default():
+
+    logger = log.make_logger()
+
+    assert logger.name == "rtestbench._logger"
+    assert logger.level == DEBUG
+
+    assert logger.handlers[0].level == INFO
+    assert logger.handlers[1].level == DEBUG
+
+
 def test_make_logger_verbose():
     """Tests that the verbose logger has the correct name and levels."""
 
