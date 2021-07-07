a1923bddb025120c896bff8d741c8f2b4c0f39d0
Alexandre QUENON <alexandre.quenon@umons.ac.be>
2021-07-07 06:16:54 -0400

    NEW oscilloscope interface: prototype of minimum requirements for daughter classes of oscillscopes.

--- rtestbench/tools/oscilloscope.py
+++ rtestbench/tools/oscilloscope.py
@@ -10,4 +10,47 @@ class Oscilloscope(Tool):
     """
 
 
-    pass
+    def __init__(self, info):
+        info.family = "oscilloscope"
+
+        Tool.__init__(self, info)
+    
+
+    # Coupling
+    def set_coupling(self, channel, mode):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+    def get_coupling(self, channel):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    # Probe attenuation factor
+    def set_probe_attenuation(self, channel, attenuation_factor):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+    def get_probe_attenuation(self, channel):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    # Timebase, scale and offset interface
+    def autoscale(self):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    def set_timebase_mode(self, mode):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    def set_timebase_scale(self, scale):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+    def set_timebase_offset(self, offset):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    def set_yscale(self, channel, scale):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+    def set_yoffset(self, channel, offset):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    # Trigger interface
+    def set_trigger_source(self, source_name: str):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+    def get_trigger_source(self) -> str:
+        raise NotImplementedError("This function must be implemented in daughter classes.")
+
+    # Measurement actions interface
+    def fetch_data(self, source_name):
+        raise NotImplementedError("This function must be implemented in daughter classes.")
